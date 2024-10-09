f5efb5f5a9d14e6bc561dd528aee67e03882edda
viettran27 <viettranduc8996@gmail.com>
2024-10-09 16:16:35 +0700

    change mst to varchar

--- be/app/api/api_tglv.py
+++ be/app/api/api_tglv.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, File, UploadFile, Depends
 from app.helper.table import export_columns, execute_filtered_query, validate_and_import_excel
-from sqlalchemy import  DECIMAL, INTEGER, NVARCHAR, VARCHAR, NUMERIC, DATE
+from sqlalchemy import  DECIMAL, NVARCHAR, VARCHAR, NUMERIC, DATE
 from app.db.base import get_db_2, engine_2
 from sqlalchemy.orm import Session
 from datetime import date
@@ -24,7 +24,7 @@ async def filter_cn_cat(db: Session = Depends(get_db_2), NGAY: date | None = Non
 @router.post("/")
 async def upload_excel(file: UploadFile = File(...)):    
     dtype = {
-        "MST": INTEGER,
+        "MST": VARCHAR(20),
         "HO_TEN": NVARCHAR(255),
         "CHUYEN": VARCHAR(100),
         "NGAY": DATE,
@@ -34,7 +34,6 @@ async def upload_excel(file: UploadFile = File(...)):
     }
 
     columns_to_validate = {
-        "MST": "integer",
         "NGAY": "date",
         "SO_GIO": "number",
         "OT": "number"
