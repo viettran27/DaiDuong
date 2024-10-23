d0824e7e82195fe24c5806033a5e81478f60e773
viettran27 <viettranduc8996@gmail.com>
2024-10-23 14:55:41 +0700

    update code

--- fe/src/components/Header.jsx
+++ fe/src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <div className='sticky w-full z-30 top-0 bg-white flex justify-center items-center py-2 flex-col shadow-2xl'>
-            <h1 className='text-[#4e73df] text-3xl font-bold'>ĐẠI DƯƠNG</h1>
+            <h1 className='text-[#4e73df] text-3xl font-bold'>TẦNG 2</h1>
             <h2 className='text-[#4e73df] text-xl font-bold'>PHẦN MỀM QUẢN LÝ</h2>
         </div>
     )
--- fe/src/constants/routes.jsx
+++ fe/src/constants/routes.jsx
@@ -10,54 +10,54 @@ import SL_CN_QC2 from "src/pages/SL_CN_QC2"
 import TGLV from "src/pages/TGLV"
 
 export const routes = [
-    {
-        name: "Nhân sự",
-        path: "/hr",
-        component: <HR/>
-    },
+    // {
+    //     name: "Nhân sự",
+    //     path: "/hr",
+    //     component: <HR/>
+    // },
     {
         name: "Sản lượng ETS",
         path: "/qty",
         component: <QTY_ETS />
     },
-    {
-        name: "Sản lượng công nhân cắt",
-        path: "/sl_cn_cat",
-        component: <SL_CN_CAT />
-    },
-    {
-        name: "Thời gian làm việc",
-        path: "/tglv",
-        component: <TGLV />
-    },
-    {
-        name: "Công nhân may đi hỗ trợ",
-        path: "/cn_di_ho_tro",
-        component: <CN_DI_HO_TRO />
-    },
-    {
-        name: "Sản lượng cá nhân",
-        path: "/sl_ca_nhan",
-        component: <SL_CA_NHAN />
-    },
+    // {
+    //     name: "Sản lượng công nhân cắt",
+    //     path: "/sl_cn_cat",
+    //     component: <SL_CN_CAT />
+    // },
+    // {
+    //     name: "Thời gian làm việc",
+    //     path: "/tglv",
+    //     component: <TGLV />
+    // },
+    // {
+    //     name: "Công nhân may đi hỗ trợ",
+    //     path: "/cn_di_ho_tro",
+    //     component: <CN_DI_HO_TRO />
+    // },
+    // {
+    //     name: "Sản lượng cá nhân",
+    //     path: "/sl_ca_nhan",
+    //     component: <SL_CA_NHAN />
+    // },
     {
         name: "Sản lượng QC may",
         path: "/sl_qc1",
         component: <SL_CN_QC1 />
     },
-    {
-        name: "Sản lượng là",
-        path: "/sl_la",
-        component: <SL_CN_LA />
-    },
-    {
-        name: "Sản lượng QC là",
-        path: "/sl_qc2",
-        component: <SL_CN_QC2 />
-    },
-    {
-        name: "Sản lượng Poly",
-        path: "/sl_cn_poly",
-        component: <SL_CN_POLY />
-    }
+    // {
+    //     name: "Sản lượng là",
+    //     path: "/sl_la",
+    //     component: <SL_CN_LA />
+    // },
+    // {
+    //     name: "Sản lượng QC là",
+    //     path: "/sl_qc2",
+    //     component: <SL_CN_QC2 />
+    // },
+    // {
+    //     name: "Sản lượng Poly",
+    //     path: "/sl_cn_poly",
+    //     component: <SL_CN_POLY />
+    // }
 ]
